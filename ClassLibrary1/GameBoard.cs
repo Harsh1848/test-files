29b7377990110f252cf26ab9d6bfc1d2baf8fa88
Harsh Patel <122567137+Harsh1848@users.noreply.github.com>
2023-08-13 23:50:12 -0400

    Add files via upload

---
+++ ClassLibrary1/GameBoard.cs
@@ -0,0 +1,11 @@
+﻿namespace TicTacToe_Game.Models
+{
+    public class GameBoard
+    {
+        public char[,] Board { get; set; } = new char[3, 3];
+        public char CurrentPlayer { get; set; } = 'X';
+        public string Winner { get; set; } = null;
+    }
+}
+
+
